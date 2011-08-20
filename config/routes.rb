dae271434b6ea2e22132954100e122d6f895f7e4
Michael Schwartz <mikalan2001@gmail.com>
2011-08-20 03:17:09 -0400

    Modified Routes and deleted index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 SampleApp2::Application.routes.draw do
-  get "pages/home"
 
-  get "pages/contact"
+  match '/contact', :to => 'pages#contact'
 
-  get "pages/about"
+  match '/about', :to => 'pages#about'
 
-  get "pages/help"
+  match '/help', :to => 'pages#help'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -56,7 +55,8 @@ SampleApp2::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+
+  root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
